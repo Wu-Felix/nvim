d574ebc9c26745ac0351ce8f0475e340feeec865
zhengfei.wu <zhengfei.wu@ujoin-tech.com>
2024-01-19 20:03:10 +0800

    checkout keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,32 +2,19 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 -- This file is automatically loaded by lazyvim.config.init
-
-local mode_nv = { "n", "v" }
-local mode_v = { "v" }
-local mode_i = { "i" }
-local nmappings = {
-  -- Window & splits
-  { from = "qf", to = "<C-w>o" },
-  { from = "<up>", to = ":res +5<CR>" },
-  { from = "<down>", to = ":res -5<CR>" },
-  { from = "<left>", to = ":vertical resize-5<CR>" },
-  { from = "<right>", to = ":vertical resize+5<CR>" },
-  -- { from = "sh",            to = "se", },
-  -- { from = "sh",            to = "<C-w>t<C-w>K", },
-  -- { from = "sv",            to = "<C-w>t<C-w>H", },
-  { "n", from = "gp", to = "<cmd>Lspsaga peek_definition<CR>", mode = mode_i },
-  { "n", from = "<leader>ca", to = "<cmd>Lspsaga code_action<CR>", mode = mode_i },
-  { "n", from = "[d", to = "<cmd>Lspsaga diagnostic_jump_prev<CR>", mode = mode_i }, -- jump to previous diagnostic in buffer
-  { "n", from = "]d", to = "<cmd>Lspsaga diagnostic_jump_next<CR>", mode = mode_i },
-  -- Other
-  { from = "jk", to = "<esc>", mode = mode_i },
-
-  -- Joshuto
-}
--- vim.keymap.del("n", "<Leader>gg", { noremap = true, silent = true })
+local key = vim.keymap
 vim.keymap.set("n", "gb", "<Cmd>BufferLinePick<CR>", { noremap = true, silent = true })
+key.set("i", "jk", "<esc>")
 
-for _, mapping in ipairs(nmappings) do
-  vim.keymap.set(mapping.mode or "n", mapping.from, mapping.to, { noremap = true })
-end
+-- windown
+key.set("n", "qf", "<C-w>o")
+key.set("n", "<up>", ":res +5<CR>", { noremap = true, silent = true })
+key.set("n", "<down>", ":res -5<CR>", { noremap = true, silent = true })
+key.set("n", "<left>", ":vertical resize-5<CR>", { noremap = true, silent = true })
+key.set("n", "<right>", ":vertical resize+5<CR>", { noremap = true, silent = true })
+
+-- Lspsaga
+key.set("n", "gp", "<cmd>Lspsaga peek_definition<CR>")
+key.set("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>")
+key.set("n", "[d", "<cmd>Lspsaga diagnostic_jump_prev<CR>") -- jump to previous diagnostic in buffer
+key.set("n", "]d", "<cmd>Lspsaga diagnostic_jump_next<CR>")
