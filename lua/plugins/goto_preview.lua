596eafeb0dba8b3fcf7dd534d78c57f6ffb5a779
Wu-Felix <Wu-Felix@example.com>
2024-02-27 20:49:37 +0800

    del sgae

--- lua/plugins/goto_preview.lua
+++ lua/plugins/goto_preview.lua
@@ -10,7 +10,12 @@ return {
       debug = false,
       opacity = nil,
       post_open_hook = nil,
+      vim.cmd("nnoremap gpd <cmd>lua require('goto-preview').goto_preview_definition()<CR>"),
+      vim.cmd("nnoremap gpt <cmd>lua require('goto-preview').goto_preview_type_definition()<CR>"),
+      vim.cmd("nnoremap gpi <cmd>lua require('goto-preview').goto_preview_implementation()<CR>"),
+      vim.cmd("nnoremap gpD <cmd>lua require('goto-preview').goto_preview_declaration()<CR>"),
       vim.cmd("nnoremap gP <cmd>lua require('goto-preview').close_all_win()<CR>"),
+      vim.cmd("nnoremap gpr <cmd>lua require('goto-preview').goto_preview_references()<CR>"),
       -- You can use "default_mappings = true" setup option
       -- Or explicitly set keybindings
       -- vim.cmd("nnoremap gpd <cmd>lua require('goto-preview').goto_preview_definition()<CR>")
