9aab19bbd4a0781c751f3b506659d467253fdf07
felix <Wu-Felix@outlook.com>
2024-03-21 18:00:51 +0800

    add markmap

--- lua/plugins/disable.lua
+++ lua/plugins/disable.lua
@@ -1,3 +1,4 @@
 return {
   { "folke/which-key.nvim", enabled = false },
+  { "lukas-reineke/headlines.nvim", enabled = false },
 }
--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -7,6 +7,20 @@ return {
     end,
   },
   {
+    "lukas-reineke/headlines.nvim",
+    config = function()
+      require("headlines").setup({
+        org = {},
+      })
+    end,
+    -- config = function()
+    --   vim.cmd([[highlight Headline1 guibg=#D19A66]])
+    --   vim.cmd([[highlight Headline2 guibg=#21262d]])
+    --   vim.cmd([[highlight CodeBlock guibg=#1c1c1c]])
+    --   vim.cmd([[highlight Dash guibg=#D19A66 gui=bold]])
+    -- end,
+  },
+  {
     "Zeioth/markmap.nvim",
     build = "yarn global add markmap-cli",
     cmd = { "MarkmapOpen", "MarkmapSave", "MarkmapWatch", "MarkmapWatchStop" },
